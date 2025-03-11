02e552fcbd05efea01beaf26c8ef2aad0e4a07d3
Mesh <bhalap@uw.edu>
2025-03-10 19:15:34 -0700

    Storing name in chrome storage

--- Client/name.js
+++ Client/name.js
@@ -1,15 +1,22 @@
 document.addEventListener("DOMContentLoaded", () => {
-    const saveNameButton = document.getElementById("saveNameButton");
-    const userNameInput = document.getElementById("userName");
-  
-    saveNameButton.addEventListener("click", () => {
-      const userName = userNameInput.value.trim();
-      if (userName) {
-        chrome.storage.sync.set({ userName: userName }, () => {
-          window.location.href = "index.html";
-        });
-      } else {
-        alert("Please enter your name.");
-      }
-    });
+  const saveNameButton = document.getElementById("saveNameButton");
+  const userNameInput = document.getElementById("userName");
+
+  // Check if username exists in Chrome storage
+  chrome.storage.sync.get(['userName'], (result) => {
+    if (result.userName) {
+      window.location.href = "index.html";
+    }
   });
+
+  saveNameButton.addEventListener("click", () => {
+    const userName = userNameInput.value.trim();
+    if (userName) {
+      chrome.storage.sync.set({ userName: userName }, () => {
+        window.location.href = "index.html";
+      });
+    } else {
+      alert("Please enter your name.");
+    }
+  });
+});
